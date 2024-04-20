c9b9aa2626d42bd6b8bb9f07f7fb4e753156f270
vigneshaa29 <vignesharumugam2910@gmail.com>
2024-04-20 14:02:31 +0530

    1st project

---
+++ BusReservationApplication/src/BusResv/DbConnection.java
@@ -0,0 +1,14 @@
+package BusResv;
+
+import java.sql.*;
+
+public class DbConnection {
+	private static final String url = "jdbc:mysql://localhost:3306/projectsdb";
+	private static final String userName = "root";
+	private static final String passWord = "Vignesh@29";
+	
+	public static Connection getConnection() throws SQLException{
+		return  DriverManager.getConnection(url,userName,passWord);
+	}
+
+}
